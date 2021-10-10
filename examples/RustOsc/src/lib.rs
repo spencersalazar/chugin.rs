55ecbe59b9d781c452d66ed6a86e8c6504477530
Spencer Salazar <spencer.salazar@output.com>
2021-10-09 21:33:09 -0700

    rework directory layout

---
+++ examples/RustOsc/src/lib.rs
@@ -0,0 +1,100 @@
+use chugin;
+use chugin::chuck;
+
+static mut DATA_OFFSET: usize = 0;
+
+/// data for the actual object itself
+#[derive(Debug)]
+struct MyChugin {
+    freq: f32,
+    amp: f32,
+    phase: f32,
+    srate: f32,
+    phase_update: f32,
+}
+
+impl MyChugin {
+    pub fn new(srate: f32, freq: f32, amp: f32) -> MyChugin {
+        MyChugin {
+            srate: srate,
+            freq: freq,
+            amp: amp,
+            phase: 0.0,
+            phase_update: freq / srate,
+        }
+    }
+
+    fn update_phase_update(&mut self) {
+        self.phase_update = self.freq / self.srate;
+    }
+
+    pub fn set_freq(&mut self, freq: f32) -> f32 {
+        self.freq = freq;
+        self.update_phase_update();
+        self.freq
+    }
+
+    pub fn get_freq(&self) -> f32 {
+        self.freq
+    }
+
+    pub fn tick(&mut self) -> f32 {
+        let y = -1.0 + self.phase * 2.0;
+        self.phase += self.phase_update;
+        if self.phase > 1.0 {
+            self.phase -= 1.0;
+        }
+        y * self.amp
+    }
+}
+
+chugin::ctor!(ctor, DATA_OFFSET, {
+    let obj = MyChugin::new(44100.0, 200.0, 1.0);
+    obj
+});
+
+chugin::dtor!(dtor, DATA_OFFSET, MyChugin, obj, {});
+
+chugin::tick!(tick, DATA_OFFSET, MyChugin, obj, _inp, { obj.tick() });
+
+chugin::mfun_setter_getter_float!(
+    set_freq,
+    get_freq,
+    DATA_OFFSET,
+    MyChugin,
+    obj,
+    freq,
+    {
+        obj.set_freq(freq as f32);
+    },
+    { obj.get_freq() }
+);
+
+fn ck_query_impl(query: *mut chuck::DL_Query) -> chugin::CKResult {
+    let q = chugin::Query::new(query)?;
+
+    q.begin_class("RustOsc", "UGen")?;
+
+    q.add_ctor(Some(ctor))?;
+    q.add_dtor(Some(dtor))?;
+
+    let offset = q.add_mvar("int", "@data", false)? as usize;
+    unsafe {
+        DATA_OFFSET = offset;
+    }
+
+    q.add_ugen_func(Some(tick), 0, 1)?;
+
+    q.add_mfun(
+        Some(set_freq),
+        "float",
+        "freq",
+        &[(String::from("float"), String::from("f"))],
+    )?;
+
+    q.end_class()?;
+
+    Ok(())
+}
+
+chugin::query!(query, ck_query_impl(query));
--- src/lib.rs
+++ src/lib.rs
@@ -1,100 +1,30 @@
-use chugin;
-use chugin::chuck;
+pub mod chuck;
+pub mod cktype;
+mod cstring;
+pub mod fn_macros;
+pub mod query;
+pub mod util;
 
-static mut DATA_OFFSET: usize = 0;
+use std::result::Result;
 
-/// data for the actual object itself
-#[derive(Debug)]
-struct MyChugin {
-    freq: f32,
-    amp: f32,
-    phase: f32,
-    srate: f32,
-    phase_update: f32,
+// re-export
+pub use cktype::CKType;
+pub use query::Query;
+
+// chuck version is #define-d, so not supported by bindgen
+// major version must be the same between chuck:chugin
+const CK_DLL_VERSION_MAJOR: chuck::t_CKUINT = 0x0008;
+// minor version of chugin must be less than or equal to chuck's
+const CK_DLL_VERSION_MINOR: chuck::t_CKUINT = 0x0000;
+
+fn ck_dll_version_make(maj: chuck::t_CKUINT, min: chuck::t_CKUINT) -> chuck::t_CKUINT {
+    (maj << 16) | min
 }
 
-impl MyChugin {
-    pub fn new(srate: f32, freq: f32, amp: f32) -> MyChugin {
-        MyChugin {
-            srate: srate,
-            freq: freq,
-            amp: amp,
-            phase: 0.0,
-            phase_update: freq / srate,
-        }
-    }
-
-    fn update_phase_update(&mut self) {
-        self.phase_update = self.freq / self.srate;
-    }
-
-    pub fn set_freq(&mut self, freq: f32) -> f32 {
-        self.freq = freq;
-        self.update_phase_update();
-        self.freq
-    }
-
-    pub fn get_freq(&self) -> f32 {
-        self.freq
-    }
-
-    pub fn tick(&mut self) -> f32 {
-        let y = -1.0 + self.phase * 2.0;
-        self.phase += self.phase_update;
-        if self.phase > 1.0 {
-            self.phase -= 1.0;
-        }
-        y * self.amp
-    }
+/// Return Chugin API version; to be returned from a public ck_version function
+pub fn version() -> chuck::t_CKUINT {
+    ck_dll_version_make(CK_DLL_VERSION_MAJOR, CK_DLL_VERSION_MINOR)
 }
 
-chugin::ctor!(ctor, DATA_OFFSET, {
-    let obj = MyChugin::new(44100.0, 200.0, 1.0);
-    obj
-});
-
-chugin::dtor!(dtor, DATA_OFFSET, MyChugin, obj, {});
-
-chugin::tick!(tick, DATA_OFFSET, MyChugin, obj, _inp, { obj.tick() });
-
-chugin::mfun_setter_getter_float!(
-    set_freq,
-    get_freq,
-    DATA_OFFSET,
-    MyChugin,
-    obj,
-    freq,
-    {
-        obj.set_freq(freq as f32);
-    },
-    { obj.get_freq() }
-);
-
-fn ck_query_impl(query: *mut chuck::DL_Query) -> chugin::CKResult {
-    let q = chugin::Query::new(query)?;
-
-    q.begin_class("RustOsc", "UGen")?;
-
-    q.add_ctor(Some(ctor))?;
-    q.add_dtor(Some(dtor))?;
-
-    let offset = q.add_mvar("int", "@data", false)? as usize;
-    unsafe {
-        DATA_OFFSET = offset;
-    }
-
-    q.add_ugen_func(Some(tick), 0, 1)?;
-
-    q.add_mfun(
-        Some(set_freq),
-        "float",
-        "freq",
-        &[(String::from("float"), String::from("f"))],
-    )?;
-
-    q.end_class()?;
-
-    Ok(())
-}
-
-chugin::query!(query, ck_query_impl(query));
+/// Chugin result type
+pub type CKResult<T = (), E = &'static str> = Result<T, E>;
