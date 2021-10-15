5e3dc126ca79e9a6eb668c8c664d6aa983ec1dd7
Spencer Salazar <spencer.salazar@gmail.com>
2021-10-15 15:58:41 -0700

    fix for split into modules

--- examples/Korg35Filter/src/lib.rs
+++ examples/Korg35Filter/src/lib.rs
@@ -1,170 +1,12 @@
 use chugin;
 use chugin::chuck;
 
+mod korg35filter;
+mod vaonepole;
+
+use korg35filter::{Float, Sample, Korg35Filter};
+
 static mut DATA_OFFSET: usize = 0;
-
-type Float = f32;
-type Sample = Float;
-const PI: Float = std::f32::consts::PI;
-
-#[derive(Debug)]
-struct VAOnePole {
-    a: Float,
-    b: Float,
-    z1: Sample,
-    srate: Float,
-    is_lpf: bool,
-}
-
-impl VAOnePole {
-    pub fn new(srate: Float) -> VAOnePole {
-        VAOnePole {
-            srate: srate,
-            a: 1.0,
-            b: 1.0,
-            z1: 0.0,
-            is_lpf: true,
-        }
-    }
-
-    pub fn tick(&mut self, xn: Sample) -> Sample {
-        // calculate v(n)
-        let vn = (xn - self.z1)*self.a;
-        
-        // form LP output
-        let lpf = vn + self.z1;
-        
-        // update memory
-        self.z1 = vn + lpf;
-        
-        let hpf = xn - lpf;
-    
-        if self.is_lpf {
-            return lpf;
-        } else {
-            return hpf;            
-        }
-    }
-    
-    pub fn get_feedback(&self) -> Sample {
-        self.z1*self.b
-    }
-
-    pub fn set_freq(&mut self, freq: Float) {
-        let freq = freq.clamp(10.0, self.srate*0.5);
-        
-        #[allow(non_snake_case)]
-        let T = 1.0/self.srate;
-        
-        let wd = 2.0*PI*freq;
-        let wa = (2.0/T)*(wd*T/2.0).tan();
-        let g = wa*T/2.0;
-        
-        self.a = g/(1.0 + g);
-    }
-}
-
-/// Built-in or custom saturation in the filter's feedback loop
-#[derive(Debug)]
-enum Saturator {
-    None,
-    Tanh,
-    Custom(fn (x: Float)->Float),
-}
-
-#[derive(Debug)]
-#[allow(non_snake_case)]
-struct Korg35Filter {
-    srate: Float,
-    freq: Float,
-    K: Float,
-    lpf1: VAOnePole,
-    lpf2: VAOnePole,
-    hpf: VAOnePole,
-    a0: Float,
-    K_norm: Float,
-    saturation: Float,
-    saturator: Saturator,
-}
-
-impl Korg35Filter {
-    pub fn new(srate: Float) -> Korg35Filter {
-        let mut k35 = Korg35Filter {
-            srate: srate,
-            freq: 100.0,
-            K: 1.0,
-            lpf1: VAOnePole::new(srate),
-            lpf2: VAOnePole::new(srate),
-            hpf: VAOnePole::new(srate),
-            a0: 1.0,
-            K_norm: 1.0,
-            saturation: 1.0,
-            saturator: Saturator::Tanh,
-        };
-        
-        // sane defaults
-        k35.set(500.0, 1.5);
-        
-        k35
-    }
-    
-    #[allow(non_snake_case)]
-    pub fn set(&mut self, freq: Float, K: Float) {
-        let freq = freq.clamp(10.0, self.srate/2.0);
-        
-        self.freq = freq;
-        self.K = K;
-        
-        #[allow(non_snake_case)]
-        let T = 1.0/self.srate;
-        
-        let wd = 2.0*PI*freq;
-        let wa = (2.0/T)*(wd*T/2.0).tan();
-        let g = wa*T/2.0;
-        
-        #[allow(non_snake_case)]
-        let G = g/(1.0 + g);
-        
-        self.lpf1.a = G;
-        self.lpf2.a = G;
-        self.hpf.a = G;
-        
-        self.lpf2.b = (K - K*G)/(1.0 + g);
-        self.hpf.b = -1.0/(1.0 + g);
-        
-        self.a0 = 1.0/(1.0-K*G+K*G*G);
-        
-        if K > 0.0 {
-            self.K_norm = 1.0/K;
-        } else {
-            self.K_norm = 1.0;
-        }
-    }
-    
-    pub fn tick(&mut self, xn: Sample) -> Sample {
-        let y1 = self.lpf1.tick(xn);
-        
-        #[allow(non_snake_case)]
-        let S35 = self.hpf.get_feedback() + self.lpf2.get_feedback();
-        
-        let u = self.a0*(y1+S35);
-        
-        let u = match self.saturator {
-            Saturator::None => u,
-            Saturator::Tanh => (u*self.saturation).tanh(),
-            Saturator::Custom(f) => f(u*self.saturation),
-        };
-        
-        let y = self.K*self.lpf2.tick(u);
-        
-        self.hpf.tick(y);
-        
-        let y = self.K_norm*y;
-        
-        return y;
-    }
-}
-
 
 chugin::ctor!(ctor, DATA_OFFSET, {
     let obj = Korg35Filter::new(44100.0);
@@ -173,7 +15,9 @@ chugin::ctor!(ctor, DATA_OFFSET, {
 
 chugin::dtor!(dtor, DATA_OFFSET, Korg35Filter, _obj, {});
 
-chugin::tick!(tick, DATA_OFFSET, Korg35Filter, obj, inp, { obj.tick(inp) });
+chugin::tick!(tick, DATA_OFFSET, Korg35Filter, obj, inp, { 
+    obj.tick(inp as Sample) as f32
+});
 
 chugin::mfun_setter_getter_float!(
     set_freq,
@@ -183,9 +27,9 @@ chugin::mfun_setter_getter_float!(
     k35,
     freq,
     {
-        k35.set(freq as Float, k35.K);
+        k35.set(freq as Float, k35.get_K() );
     },
-    { k35.freq }
+    { k35.get_freq() }
 );
 
 chugin::mfun_setter_getter_float!(
@@ -196,9 +40,9 @@ chugin::mfun_setter_getter_float!(
     k35,
     k,
     {
-        k35.set(k35.freq, k as Float );
+        k35.set(k35.get_freq(), k as Float );
     },
-    { k35.K }
+    { k35.get_K() }
 );
 
 fn ck_query_impl(query: *mut chuck::DL_Query) -> chugin::CKResult {
